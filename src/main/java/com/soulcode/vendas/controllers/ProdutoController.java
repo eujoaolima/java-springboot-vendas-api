05c0f3a2416d278a2524f7fbe4a735f52979c0ba
João Lima <joaopedro.donatolima@hotmail.com>
2023-07-27 20:38:02 -0300

    Finalização do CRUD de produtos

--- src/main/java/com/soulcode/vendas/controllers/ProdutoController.java
+++ src/main/java/com/soulcode/vendas/controllers/ProdutoController.java
@@ -1,8 +1,13 @@
 package com.soulcode.vendas.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,23 +19,60 @@ import com.soulcode.vendas.repositories.ProdutoRepository;
 public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
-    
-    // @RequestMapping(value = "/produtos", method = RequestMethod.GET)
-    // public Produto save() {
-    //     Produto produto = new Produto();
-    //     produto.setCodigo("123456");
-    //     produto.setDescricao("Batata");
-    //     produto.setPreco(6F);
-    //     produto.setUnidadeMedida("L");
-    //     produto.setGrupo("Vegetais");
-    //     produto = this.produtoRepository.save(produto);
-    //     return produto;
-    // }
 
-    @RequestMapping(value="/produtos", method = RequestMethod.GET)
+    @RequestMapping(value = "/produtos", method = RequestMethod.POST)
+    public Produto save(@RequestBody Produto produto) {
+        produto = this.produtoRepository.save(produto);
+        return produto;
+    }
+
+    @RequestMapping(value = "/produtos", method = RequestMethod.GET)
     public List<Produto> findAll() {
         List<Produto> produtos = this.produtoRepository.findAll();
         return produtos;
     }
 
+    @RequestMapping(value = "/produtos/{id}", method = RequestMethod.GET)
+    public Produto findById(@PathVariable Long id) {
+        Optional<Produto> resultado = this.produtoRepository.findById(id);
+        if (resultado.isEmpty()) {
+            throw new RuntimeException("O produto não foi encontrado :(");
+        } else {
+            return resultado.get();
+        }
+    }
+
+    @RequestMapping(value = "/produtos/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Produto> deleteById(@PathVariable Long id) {
+        Produto produto = findById(id);
+        if (produto == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            this.produtoRepository.deleteById(id);
+            return new ResponseEntity<>(produto, HttpStatus.OK);
+        }
+    }
+
+    @RequestMapping(value = "/produtos", method = RequestMethod.DELETE)
+    public ResponseEntity<List<Produto>> deleteAll() {
+        List<Produto> produto = findAll();
+        if (produto == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            this.produtoRepository.deleteAll();
+            return new ResponseEntity<>(produto, HttpStatus.OK);
+        }
+    }
+
+    @RequestMapping(value = "/produtos/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Produto> updateById(@PathVariable Long id, @RequestBody Produto produto) {
+        this.findById(id);
+        if (produto == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            produto.setId(id);
+            produto = this.produtoRepository.save(produto);
+            return new ResponseEntity<>(produto, HttpStatus.OK);
+        }
+    }
 }
